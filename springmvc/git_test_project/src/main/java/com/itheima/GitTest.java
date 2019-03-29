368106f389646520050b26b332c1e9a1c9de4e94
yuhuan20 <25700987@qq.com>
2019-03-29 18:37:27 +0800

    添加test工程到git

---
+++ springmvc/git_test_project/src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("shiyuanlimei");
+        System.out.println("shiyuanlimei");
+        System.out.println("shiyuanlimei");
+        System.out.println("shiyuanlimei");
+        System.out.println("shiyuanlimei");
+    }
+}
